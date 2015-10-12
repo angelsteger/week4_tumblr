744d70bbf95b25a78baaabd8b85f4723ccf6a888
Angel Steger <angel.steger@gmail.com>
2015-10-11 21:37:25 -0400

    wernjwekrh
    kwejlwkjre

---
+++ Week 4 Tumblr/Week 4 Tumblr/ComposeViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ComposeViewController.swift
+//  Week 4 Tumblr
+//
+//  Created by Angel Steger on 10/10/15.
+//  Copyright © 2015 Angel Steger. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    @IBOutlet weak var textButton: UIButton!
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var quoteButton: UIButton!
+    @IBOutlet weak var linkButton: UIButton!
+    @IBOutlet weak var chatButton: UIButton!
+    @IBOutlet weak var videoButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textButton.center.y = 560
+        photoButton.center.y = 560
+        quoteButton.center.y = 560
+        linkButton.center.y = 560
+        chatButton.center.y = 560
+        videoButton.center.y = 560
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear (animated: Bool) {
+        //
+    }
+    override func viewDidAppear (animated: Bool) {
+        
+        
+    }
+    override func viewWillDisappear (animated: Bool) {
+    }
+    
+    @IBAction func onTapNevermindButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Week 4 Tumblr/Week 4 Tumblr/ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  Week 4 Tumblr
+//
+//  Created by Angel Steger on 10/10/15.
+//  Copyright © 2015 Angel Steger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var accountButton: UIButton!
+    @IBOutlet weak var trendingButton: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+    @IBAction func tapCompose(sender: AnyObject) {
+    }
+    
+/*class MyViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    var isPresenting: Bool = true
+    ...
+    }*/
+
+    // Create variables to hold each view controller associated with a tab
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    var composeViewController: UIViewController!
+    // Create variable for an array to hold all of the view controllers
+    var viewControllers: [UIViewController]!
+    // Define a variable to keep track of the tab button that is selected, setting default selected to 0
+    var selectedIndex: Int = 0
+    var isPresenting: Bool = true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Within the ViewDidLoad() method, access the main Storyboard through code
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // Instantiates each ViewController by referencing storyboard and the particular ViewController's Storyboard ID
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        // Adds each ViewController to your viewControllers array
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        // Set the Initial Tab when the App Starts
+        buttons[selectedIndex].selected = true
+        didPressTab(buttons[selectedIndex])
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // Create action and add all the buttons here
+    @IBAction func didPressTab(sender: UIButton) {
+        let previousIndex = selectedIndex
+        selectedIndex = sender.tag
+        buttons[previousIndex].selected = false
+        let previousVC = viewControllers[previousIndex]
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        sender.selected = true
+        let vc = viewControllers[selectedIndex]
+        addChildViewController(vc)
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        vc.didMoveToParentViewController(self)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        let destinationVC = segue.destinationViewController as UIViewController
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationVC.transitioningDelegate = self
+    }
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = false
+        return self
+    }
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.3
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        print("animating transition")
+        let containerView = transitionContext.containerView()
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        if (isPresenting) {
+            containerView!.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                toViewController.view.alpha = 1
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+            }
+        } else {
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
+    }
+}
+
